f234e52a63ada40492141c2795cc63febde23960
vajagani dhanesh <vajaganidhanesh@gmail.com>
2023-03-28 06:14:27 +0530

    changing path of details

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Getdetails from "./state/Getdetails";
+import Getdetails from "./Getdetails";
 function App() {
   return (
     <Router>
